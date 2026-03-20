7785637f34cbd85a0e53c3b3009844af4e1cdd31
delioomega <deliorafaelcantillovilla@gmail.com>
2026-03-20 08:56:07 -0500

    fff

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,7 @@
+
 from .producto import producto_blueprint
 
 def cargarRutas(app):
     app.register_blueprint(producto_blueprint ,url_prefix='/producto')
     #app.register_blueprint(factura_blueprint ,url_prefix='/factura')
+
