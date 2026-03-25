b78a4405f615b8468c539dc4934d49669488c0dc
delioomega <deliorafaelcantillovilla@gmail.com>
2026-03-25 09:27:45 -0500

    gallo

--- controllers/provedores_controller.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify, request
-from services.productos_services import listado_productos
-def cnlistado_productos():
-    data=listado_productos()
-    print(data)
-    return jsonify(data)
---
+++ services/venta_services.py
@@ -0,0 +1,2 @@
+from flask import jsonify, request
+from models.ventas_model import Ventas
