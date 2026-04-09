b51cb4af0f8d79f0cd974a050f7b5829aaf4f2ba
delioomega <deliorafaelcantillovilla@gmail.com>
2026-04-08 23:24:39 -0500

    pp

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from controllers.usuario_controller import *
 
-usuario_bp = Blueprint('usuario_bp', __name__)
+usuarios_bp = Blueprint('usuarios_bp', __name__)  # ← añade la 's'
 
-usuario_bp.route('/usuarios', methods=['GET'])(cnlistado_usuarios)
-usuario_bp.route('/usuarios', methods=['POST'])(cncrear_usuario)
-usuario_bp.route('/usuarios/<string:id>', methods=['DELETE'])(cneliminar_usuario)
-usuario_bp.route('/usuarios/<string:id>', methods=['PUT'])(cnactualizar_usuario)
+usuarios_bp.route('/usuarios', methods=['GET'])(cnlistado_usuarios)
+usuarios_bp.route('/usuarios', methods=['POST'])(cncrear_usuario)
+usuarios_bp.route('/usuarios/<string:id>', methods=['DELETE'])(cneliminar_usuario)
+usuarios_bp.route('/usuarios/<string:id>', methods=['PUT'])(cnactualizar_usuario)
