8f0d9275e068e6bb54ba01dc1ee1490bc78bab59
delioomega <deliorafaelcantillovilla@gmail.com>
2026-04-10 10:03:56 -0500

    ppp

--- routes/__init__.py
+++ routes/__init__.py
@@ -8,6 +8,7 @@ from .movimientos_routes import movimiento_bp
 from .pagos_routes import pagos_bp
 from .categoria_routes import categoria_bp
 from .histrial_pedido_routes import historial_pedido_bp
+from .documentacion_routes import documentacion_bp
 
 def cargarRutas(app):
     app.register_blueprint(producto_bp, url_prefix='/productos')
@@ -20,6 +21,7 @@ def cargarRutas(app):
     app.register_blueprint(pagos_bp, url_prefix='/pagos')
     app.register_blueprint(categoria_bp, url_prefix='/categoria')
     app.register_blueprint(historial_pedido_bp, url_prefix='/historial_pedido')
+    app.register_blueprint(documentacion_bp, url_prefix='/documentacion')
     #app.register_blueprint(factura_blueprint ,url_prefix='/factura')
 
 # __int__py
